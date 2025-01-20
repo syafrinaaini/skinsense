cdc41f372acbb1fec282720a0beecea8dc0eef9d
syafrinaaini <syafrinaaini18@gmail.com>
2025-01-20 12:39:03 +0700

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+{import('tailwindcss').Config} 
+module.exports = {
+  content: [
+    "D:/skinsense/.vscode/src/index.html", 
+    "D:/skinsense/.vscode/src/index.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
